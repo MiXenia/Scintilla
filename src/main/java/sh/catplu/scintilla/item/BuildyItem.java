50435a5c327884d6d5b4167f6a9826447facb99e
MiXenia <xeraoflight@gmail.com>
2025-07-15 11:36:31 -0500

    v1.0 Release

--- src/main/java/sh/catplu/scintilla/item/BuildyItem.java
+++ src/main/java/sh/catplu/scintilla/item/BuildyItem.java
@@ -6,23 +6,28 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
+import java.util.Objects;
 
+@ParametersAreNonnullByDefault
 public class BuildyItem extends Item {
-    private final String loreID;
+    private final String  loreID;
     private final Boolean foil;
-    private final Rarity rarity;
+    private final Rarity  rarity;
+
     public BuildyItem(Properties pProperties, @Nullable String loreID, @Nullable Boolean foil, @Nullable Rarity rarity) {
         super(pProperties);
         this.loreID = loreID;
-        this.foil = foil;
+        this.foil   = foil;
         this.rarity = rarity;
     }
 
     @Override
-    public void appendHoverText(ItemStack pStack, @org.jetbrains.annotations.Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
         if (loreID != null) {
             pTooltipComponents.add(Component.translatable("tooltip.scintilla." + loreID));
         }
@@ -31,19 +36,11 @@ public class BuildyItem extends Item {
 
     @Override
     public boolean isFoil(ItemStack pStack) {
-        if (foil != null) {
-            return foil;
-        } else {
-            return super.isFoil(pStack);
-        }
+        return Objects.requireNonNullElseGet(foil, () -> super.isFoil(pStack));
     }
 
     @Override
-    public Rarity getRarity(ItemStack pStack) {
-        if (rarity != null) {
-            return rarity;
-        } else {
-            return super.getRarity(pStack);
-        }
+    public @NotNull Rarity getRarity(ItemStack pStack) {
+        return Objects.requireNonNullElseGet(rarity, () -> super.getRarity(pStack));
     }
 }
